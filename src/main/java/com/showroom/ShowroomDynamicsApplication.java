7da9f710e3d0716223416229d0a7596c98cb3efa
MaheshLavand <maheshlavand1107@gmail.com>
2024-09-04 14:56:12 +0530

    first commit

---
+++ src/main/java/com/showroom/ShowroomDynamicsApplication.java
@@ -0,0 +1,13 @@
+package com.showroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowroomDynamicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowroomDynamicsApplication.class, args);
+	}
+
+}
